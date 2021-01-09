35ee496425ea99e913403e52f1913bded1d1f23e
AngelRosasC <64498063+AngelRosasC@users.noreply.github.com>
2021-01-08 21:55:11 -0600

    implementando los clientes retrofit

--- app/src/main/java/com/arcode/moviesfeed/http/MoviesApiService.java
+++ app/src/main/java/com/arcode/moviesfeed/http/MoviesApiService.java
@@ -4,10 +4,9 @@ import com.arcode.moviesfeed.http.apimodel.TopMoviesRated;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface MoviesApiService {
     @GET("top_rated")
-    Call<TopMoviesRated> getTopMoviesRated(@Query("api_key") String api_key, @Query("page") int page);
+    Call<TopMoviesRated> getTopMoviesRated(@Query("page") int page);
 }
--- app/src/main/java/com/arcode/moviesfeed/http/MoviesTitleModule.java
+++ app/src/main/java/com/arcode/moviesfeed/http/MoviesTitleModule.java
@@ -2,8 +2,13 @@ package com.arcode.moviesfeed.http;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class MoviesTitleModule {
@@ -12,12 +17,27 @@ public class MoviesTitleModule {
 
     @Provides
     public OkHttpClient provideOkHttpClient() {
-        return null;
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        return new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .addInterceptor((chain -> {
+                    Request request = chain.request();
+                    HttpUrl url = request.url().newBuilder().addQueryParameter("api_key", API_KEY).build();
+                    request = request.newBuilder().url(url).build();
+                    return chain.proceed(request);
+                }))
+                .build();
     }
 
     @Provides
     public Retrofit provideRetrofit(String baseUrl, OkHttpClient client) {
-        return null;
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
     }
 
     @Provides
